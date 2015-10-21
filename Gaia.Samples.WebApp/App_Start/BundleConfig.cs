13cf8fdf55ddab980669cbb5447fe4f8aa2a7443
stepanfryd <stepan.fryd@gmail.com>
2015-10-21 17:29:25 +0100

    Some upgrades

---
+++ Gaia.Samples.WebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Gaia.Samples.WebApp
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+									"~/bower_components/jquery/dist/jquery.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+									"~/bower_components/jquery-validation/dist/jquery.validate.js"));
+
+			// Use the development version of Modernizr to develop with and learn from. Then, when you're
+			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+									"~/bower_components/modernizr/src/modernizr.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+								"~/bower_components/bootstrap/dist/js/bootstrap.js",
+								"~/bower_components/respond/dest/respond.min.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+								"~/bower_components/bootstrap/dist/css/bootstrap.css",
+								"~/Content/site.css"));
+		}
+	}
+}
---
+++ Gaia.Samples.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gaia.Samples.WebApp.MvcApplication" Language="C#" %>
