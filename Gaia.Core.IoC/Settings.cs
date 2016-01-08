394c65746e8244a8f879323c37b9b7fd8c3d1e98
stepanfryd <stepan.fryd@gmail.com>
2016-01-08 18:46:28 +0000

    settings

---
+++ Gaia.Core.IoC/Settings.cs
@@ -0,0 +1,28 @@
+﻿using NParametrizer;
+
+namespace Gaia.Core.IoC
+{
+	public class Settings : ParametersBase
+	{
+		#region Public members
+
+		/// <summary>
+		///   Configuration section
+		/// </summary>
+		[Config("gaia/ioc")]
+		public IoCSection ContainerSection { get; set; }
+
+		#endregion
+
+		#region Private and protected
+
+		/// <summary>
+		///   Custom parameter validation
+		/// </summary>
+		protected override void ValidateArguments()
+		{
+		}
+
+		#endregion
+	}
+}
