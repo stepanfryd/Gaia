2ef1863eb9d5ddefd6d000c32359a34c87d496d4
stepanfryd <stepan.fryd@gmail.com>
2016-01-26 13:24:05 +0000

    lifetimemanager

---
+++ Gaia.Core.IoC.Unity/LifetimeManagers/ContainerControlledLifetimeManager.cs
@@ -0,0 +1,9 @@
+﻿using Gaia.Core.IoC.LifetimeManagers;
+
+namespace Gaia.Core.IoC.Unity.LifetimeManagers
+{
+	public class ContainerControlledLifetimeManager : Microsoft.Practices.Unity.ContainerControlledLifetimeManager, ILifetimeManager
+	{
+		 
+	}
+}
