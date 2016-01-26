f61db7d3a7b1c6db46646f5cdd6a4639b8669e65
stepanfryd <stepan.fryd@gmail.com>
2016-01-26 13:22:14 +0000

    Injection members

---
+++ Gaia.Core.IoC.Unity/InjectionMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Practices.ObjectBuilder2;
+
+namespace Gaia.Core.IoC.Unity
+{
+	public class InjectionMember : Microsoft.Practices.Unity.InjectionMember
+	{
+		public override void AddPolicies(Type serviceType, Type implementationType, string name, IPolicyList policies)
+		{
+			
+		}
+	}
+}
---
+++ Gaia.Core.IoC/IInjectionMember.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Gaia.Core.IoC
+{
+	public interface IInjectionMember
+	{
+		Type Type { get; set; }
+		object Object { get; set; }
+		string Name { get; set; }
+	}
+}
