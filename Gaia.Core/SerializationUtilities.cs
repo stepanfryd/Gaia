82c7810b45c05eee62d6f38e57446766f271aef5
Stepan Fryd <stepan.fryd@gmail.com>
2016-04-13 17:45:11 +0100

    Serialization utility

---
+++ Gaia.Core/SerializationUtilities.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Gaia.Core
+{
+	/// <summary>
+	/// Serialization and deserialization utilities for different formats
+	/// </summary>
+	public static class SerializationUtilities
+	{
+		/// <summary>
+		/// Serialize object to XML string
+		/// </summary>
+		/// <param name="data"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public static string SerializeXml<T>(this T data)
+		{
+			string retVal;
+			var seri = new XmlSerializer(typeof(T));
+
+			using (var ms = new MemoryStream())
+			{
+				using (var sr = new StreamWriter(ms))
+				{
+					using (var xmlWriter = XmlWriter.Create(sr, new XmlWriterSettings { Indent = false }))
+					{
+						seri.Serialize(xmlWriter, data);
+					}
+					ms.Seek(0, SeekOrigin.Begin);
+					retVal = Encoding.Default.GetString(ms.ToArray());
+					sr.Close();
+				}
+				ms.Close();
+			}
+			return retVal;
+		}
+
+		/// <summary>
+		/// Deserialize from XML
+		/// </summary>
+		/// <param name="data"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public static T DeserialiyeXml<T>(string data)
+		{
+			T retVal;
+			var seri = new XmlSerializer(typeof(T));
+
+			using (var ms = new MemoryStream(Encoding.Default.GetBytes(data)))
+			{
+				ms.Seek(0, SeekOrigin.Begin);
+				retVal = (T)seri.Deserialize(ms);
+				ms.Close();
+			}
+			return retVal;
+		}
+
+
+	}
+}
