f5c4146b930fc46c03b777efc05f95c2658e05e0
stepanfryd <stepan.fryd@gmail.com>
2016-07-31 18:07:51 +0100

    IServiceProvider implementation

---
+++ Gaia.Core.IoC/ServiceProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Gaia.Core.IoC
+{
+	/// <summary>
+	/// </summary>
+	public class ServiceProvider : IServiceProvider
+	{
+		#region Fields and constants
+
+		private readonly IContainer _container;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// </summary>
+		/// <param name="container"></param>
+		public ServiceProvider(IContainer container)
+		{
+			_container = container;
+		}
+
+		#endregion
+
+		#region Interface Implementations
+
+		/// <summary>Gets the service object of the specified type.</summary>
+		/// <returns>
+		///   A service object of type <paramref name="serviceType" />.-or- null if there is no service object of type
+		///   <paramref name="serviceType" />.
+		/// </returns>
+		/// <param name="serviceType">An object that specifies the type of service object to get. </param>
+		/// <filterpriority>2</filterpriority>
+		public object GetService(Type serviceType)
+		{
+			return _container.Resolve(serviceType);
+		}
+
+		#endregion
+	}
+}
