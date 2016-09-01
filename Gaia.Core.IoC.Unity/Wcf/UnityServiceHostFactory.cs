375f4a0b1cdfe38e998cac82526f28b3797184dc
stepanfryd <stepan.fryd@gmail.com>
2016-09-01 23:31:04 +0200

    WCF IoC service host factory

---
+++ Gaia.Core.IoC.Unity/Wcf/UnityServiceHostFactory.cs
@@ -0,0 +1,10 @@
+﻿using Gaia.Core.IoC.Unity.Workflow;
+
+namespace Gaia.Core.IoC.Unity.Wcf
+{
+	public class UnityServiceHostFactory : Core.Wcf.IoC.IoCServiceHostFactory {
+		protected override void ConfigureContainer(IContainer container)
+		{
+		}
+	}
+}
